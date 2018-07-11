11e96bb8c21f060a7bed5a0384e0031c19f13283
jbryant32 <jason.bryant28@gmail.com>
2018-07-10 22:42:44 -0700

    UPDATED CSS ADD PANEL

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,25 +1,17 @@
-(function () {
-    var comingSoonSpan = document.getElementById("select-coming-soon");
-    var inTheatersSpan = document.getElementById("select-in-theater");
-    var highlighterForSelect = document.getElementById("selection-highlighter");
-    comingSoonSpan.click = () => { alert() }
-    comingSoonSpan.addEventListener("onClick", () => {
-        highlighterForSelect.style.left = "0%";
-        console.log("coming soon");
-    })
-    inTheatersSpan.addEventListener("click", () => {
-        highlighterForSelect.style.left = "50%";
-        console.log("in theaters");
-    })
-})();
-var highlighterForSelect = document.getElementById("selection-highlighter");
 
+var highlighterForSelect = document.getElementById("selection-highlighter");
+var comingSoon = document.getElementById("coming-soon");
+var inTheaters = document.getElementById("in-theaters");
 function slideLeft() {
     highlighterForSelect.style.left = "0%";
+    comingSoon.style.left = "0%";
+    inTheaters.style.left="-100%";
     console.log("coming soon");
 }
 
 function slideRight() {
     highlighterForSelect.style.left = "50%";
+    inTheaters.style.left="0%";
+    comingSoon.style.left = "100%";
     console.log("in theaters");
 }
